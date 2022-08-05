bf285e33d208f18e1c0b9f89f893c1baedc40629
Nikoteenie <105375963+Nikoteenie@users.noreply.github.com>
2022-08-05 11:24:21 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 
+
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        This code is 
-        <a href="https://github.com/Nikoteenie/weather-shecodes.git" alt=""> open-sourced </a>
-        by Nicole Brodkin 
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <button className="btn btn-success"> Hi</button>
+        <footer>
+          This code is
+          <a href="https://github.com/Nikoteenie/weather-shecodes.git" alt="">
+            {" "}
+            open-sourced{" "}
+          </a>
+          by Nicole Brodkin
+        </footer>
+      </div>
     </div>
   );
 }
