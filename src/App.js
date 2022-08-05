17ec5041a469f768dc4b6618a6f6a548b2f930f2
Nikoteenie <105375963+Nikoteenie@users.noreply.github.com>
2022-08-05 11:00:35 -0400

    Added Github repo link

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer>
+        This code is 
+        <a href="https://github.com/Nikoteenie/weather-shecodes.git" alt=""> open-sourced </a>
+        by Nicole Brodkin 
+      </footer>
+    </div>
+  );
 }
 
 
