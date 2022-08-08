a74345aa69933c7996e8ebcec440e547b9045b08
Nikoteenie <105375963+Nikoteenie@users.noreply.github.com>
2022-08-08 17:27:18 -0400

    edited to find issue

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,11 +1,10 @@
 import React from "react";
 
-
 export default function FormattedDate(props) {
   let days = [
     "Sunday",
     "Monday",
-    "Tueday",
+    "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
@@ -15,15 +14,16 @@ export default function FormattedDate(props) {
   let day = days[props.date.getDay()];
 
   let hours = props.date.getHours();
+
   if (hours < 10) {
     hours = `0${hours}`;
   }
 
   let minutes = props.date.getMinutes();
+
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-
   return (
     <div>
       {day} {hours}:{minutes}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature"
 
 export default function WeatherInfo(props) {
   return (
@@ -19,7 +20,10 @@ export default function WeatherInfo(props) {
               <WeatherIcon code={props.data.icon} size={52} />
             </div>
 
-            <div className="float-left">{props.data.temperature}</div>
+            <div className="float-left">
+              {" "}
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
           </div>
         </div>
         <div className="col-6">
