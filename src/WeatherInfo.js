4bc84b787deb47b6502d16cf6dbc871cde901940
Nikoteenie <105375963+Nikoteenie@users.noreply.github.com>
2022-08-09 11:02:36 -0400

    Added Temperature Conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
-import WeatherTemperature from "./WeatherTemperature"
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -17,11 +17,10 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div className="float-left">
-              <WeatherIcon code={props.data.icon} size={10} />
+              <WeatherIcon code={props.data.icon} size={52} />
             </div>
 
             <div className="float-left">
-              {" "}
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
