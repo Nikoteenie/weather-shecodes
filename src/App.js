151552187d1892464cabccaa24e5639434ab1d18
Nikoteenie <105375963+Nikoteenie@users.noreply.github.com>
2022-08-09 11:06:18 -0400

    Updated link text decoration
    and defaultcity

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Miami" />
         <footer>
           This code is
           <a href="https://github.com/Nikoteenie/weather-shecodes.git" alt="">
