ed62acaa710f2e6c0192bd25cd256fb8fc033ed6
Nikoteenie <105375963+Nikoteenie@users.noreply.github.com>
2022-08-10 13:39:16 -0400

    Included Forecast

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -28,7 +28,7 @@ export default function WeatherIcon (props){
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#1e1e1e"
-      size={54}
+      size={props.size}
       animate={true}
     />
   );
